addb0fa92beaa43f6ef018c0c0ec1e0da8d8a80d
Tomas Padilla <padillatomasagustin@gmail.com>
2022-01-20 11:05:17 +0100

    Develop - Removed Images from GIT, finished HTML

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/common/product';
-import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'app/common/product';
+import { ProductService } from 'app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
