912c0e01e1e4636d1d00dce74b9043d1f114617c
Tomas Padilla <padillatomasagustin@gmail.com>
2022-01-25 17:54:58 +0100

    Develop - Ready to continue

---
+++ src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css'],
+})
+export class CartDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
