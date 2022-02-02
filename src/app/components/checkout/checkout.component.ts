0626ec1b119df1e004f62a6b4940cf1688a19c58
Tomas Padilla <padillatomasagustin@gmail.com>
2022-02-02 12:17:44 +0100

    Checkout - Form Done, falta CSS y valores del Carrito

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css'],
+})
+export class CheckoutComponent implements OnInit {
+  // Form:
+  checkoutFormGroup: FormGroup;
+
+  totalPrice: number = 0;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    // Forming the FORM:
+    this.checkoutFormGroup = this.formBuilder.group({
+      // Customer
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: [''],
+      }),
+      // Shipping
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      // Billing
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      // Credit Card
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        cardHolder: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
+      }),
+    });
+  }
+
+  // Submit:
+  onSubmit() {
+    console.log('Submitting DATA');
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log(this.checkoutFormGroup.get('shippingAddress')?.value);
+    console.log(this.checkoutFormGroup.get('billingAddress')?.value);
+  }
+  // Shipping === Billing
+  useSameShippingAsBilling(event: any) {
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress'].setValue(
+        this.checkoutFormGroup.controls['shippingAddress'].value
+      );
+      // this.billingAddressStates = this.shippingAddressStates;
+    } else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+      // this.billingAddressStates =[];
+    }
+  }
+}
