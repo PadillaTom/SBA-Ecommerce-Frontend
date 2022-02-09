ac866d7ee7ca85ace15065e0942af09205ccf387
Tomas Padilla <padillatomasagustin@gmail.com>
2022-02-09 09:15:57 +0100

    Fixes, testing WindowLocation

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -8,12 +8,14 @@
 //   },
 // };
 
+//      redirectUri: 'https://sba-onlinestore.netlify.app/login/callback',
+
 // // === LIVE ===
 export default {
   oidc: {
     clientId: '0oa3tdhcwi4zoVPZi5d7',
     issuer: 'https://dev-11935476.okta.com/oauth2/default',
-    redirectUri: 'https://sba-onlinestore.netlify.app/login/callback',
+    redirectUri: window.location.origin + '/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
