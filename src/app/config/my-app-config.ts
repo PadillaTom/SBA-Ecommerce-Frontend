22c69aca85f1846e7e32860f3ec05c26feaaf466
Tomas Padilla <padillatomasagustin@gmail.com>
2022-02-09 19:44:23 +0100

    Fixes - Redirects File

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -15,7 +15,7 @@ export default {
   oidc: {
     clientId: '0oa3tdhcwi4zoVPZi5d7',
     issuer: 'https://dev-11935476.okta.com/oauth2/default',
-    redirectUri: window.location.origin + '/login/callback',
+    redirectUri: 'https://sba-onlinestore.netlify.app/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
